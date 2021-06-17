76d9be5b6bbc05fc727cc75e47d5db2dd5b4f4a0
Shreyash Kashyap <76607993+SYK-08@users.noreply.github.com>
2021-06-17 17:11:15 +0530

    Update healthy_programmer.py

--- healthy_programmer.py
+++ healthy_programmer.py
@@ -3,8 +3,8 @@
 #assume that a programmer works at office from 9am to 5pm.we have to take care of his health
 #and remind him three things.
 
-# 1. to drink 3.5 litres of water after some time interval between 9am to 5pm(8hours),so alarm goes on in every 28 min.
-# 2. to do eyes excersise after every 30 minutes.
+# 1. to drink water every 40 minutes.
+# 2. to do eye excersise after every 30 minutes.
 # 3. to perform physical activity after every 45 minutes.
 
 # instructions:-
@@ -13,11 +13,10 @@
 # input which implies that he has done the task.
 
 # for water user should enter 'drank'
-# for eye excersise user should enter 'EyDone'
-# for physical activity user should enter 'ExDone'
+# for eye excersise user should enter 'eyDone'
+# for physical activity user should enter 'exDone'
 
-# after the user enter the input a file should be created for every task seperately,
-# which contains the detail about time when the user performed a certain task.
+# after the user enter the input, a log file should be created listing all the tasks done.
 
 # challenges;-
 
@@ -35,6 +34,7 @@ def music_on_loop(file,stopper):
     mixer.music.play()
     while True:
       user_input = input("Type here")
+      user_input = user_input.lower()
       if user_input == stopper:
         mixer.music.stop()
         break
@@ -48,25 +48,25 @@ if __name__ == '__main__':
     initial_eyes = time()#krdiye hai
     initial_exercise = time()
 
-    water_seconds = 40*60 #40*60 means har 40 minute me.
-    eyes_seconds = 30*60 #30*60 means har 30 minute me.
-    physical_excersise_seconds = 45*60 #45*60 means har 45 minute me.
+    water_secs = 40*60 #40*60 means har 40 minute me paani pina padega.
+    eye_secs = 30*60 #30*60 means har 30 minute me eyes ka exercise karna padega.
+    phy_exc_secs = 45*60 #45*60 means har 45 minute me physical exercise karna padega.
 
     while True:
-        if time() - initial_water > water_seconds:#initial time ko current time se minus krke 40 minute se greater kare hai.
+        if time() - initial_water > water_secs:#initial time ko current time se minus krke 40 minute se greater kare hai.
             print("Water drinking time. Enter 'drank' to stop the alarm\n")
             music_on_loop("drinks_on_me.mp3","drank")
-            initial_water = time()#firse initial time set hojyga
+            initial_water = time() #firse initial time set hojyga water ka
             log("Drank water at:")
 
-        if time() - initial_eyes > eyes_seconds:
+        if time() - initial_eyes > eye_secs:
             print("Eyes excersise time. Enter 'eydone' to stop the alarm\n")
             music_on_loop("devils_eyes.mp3","eydone")
-            initial_eyes = time()
+            initial_eyes = time() #firse initial time set hojyga eye exercise ka
             log("Done eyes excersise at:")
 
-        if time() - initial_exercise > physical_excersise_seconds:
+        if time() - initial_exercise > phy_ex_secs:
             print("Physical excersise time.Enter 'exdone' to stop the alarm\n")
             music_on_loop("turn_down_for_what.mp3","exdone")
-            initial_exercise= time()
+            initial_exercise= time() #firse initial time set hojyga physical exercise ka
             log("Done physical excersise at:")
